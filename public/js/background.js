d1deec2d2e0a9d5fb455d78e4aa907e33955b9b1
Adrián Domínguez Casasola <62456423+adriandomc@users.noreply.github.com>
2025-01-16 16:48:28 -0600

    Update bg animation

--- public/js/background.js
+++ public/js/background.js
@@ -1,13 +1,31 @@
+let ellipses = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop(); // Remove this line if you want continuous animation
+  for (let i = 0; i < 100; i++) {
+    ellipses.push({
+      x: random(width),
+      y: random(height),
+      size: random(10, 50),
+      xSpeed: random(-2, 2),
+      ySpeed: random(-2, 2),
+      color: [random(255), random(255), random(255)]
+    });
+  }
 }
 
 function draw() {
   background(0);
-  for (let i = 0; i < 100; i++) {
-    fill(random(255), random(255), random(255));
-    ellipse(random(width), random(height), random(10, 50));
+  for (let i = 0; i < ellipses.length; i++) {
+    let e = ellipses[i];
+    fill(e.color);
+    ellipse(e.x, e.y, e.size);
+    e.x += e.xSpeed;
+    e.y += e.ySpeed;
+
+    // Bounce off edges
+    if (e.x < 0 || e.x > width) e.xSpeed *= -1;
+    if (e.y < 0 || e.y > height) e.ySpeed *= -1;
   }
 }
 

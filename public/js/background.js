9540161855d06f85179ca35d0759d9381fa8a292
Adrián Domínguez Casasola <62456423+adriandomc@users.noreply.github.com>
2025-01-29 16:10:53 -0600

    Update page

--- public/js/background.js
+++ public/js/background.js
@@ -1,41 +1,31 @@
-let ellipses = [];
+let stars = [];
+let x, y;
+let dx, dy;
+
+let imgDino;
+
+function preload() {
+  imgDino = loadImage("src/img/cosmicdino.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  blendMode(ADD); // Use additive blending mode for liquid light show effect
-  for (let i = 0; i < 100; i++) {
-    ellipses.push({
-      x: random(width),
-      y: random(height),
-      size: random(10, 50),
-      xSpeed: random(-2, 2),
-      ySpeed: random(-2, 2),
-      color: [random(255), random(255), random(255), 100] // Add alpha for transparency
-    });
-  }
+  
+  x = width / 2;
+  y = height / 2;
+  dx = -1;
+  dy = 0.5 * dx;
 }
 
 function draw() {
-  background(0, 20); // Use a low alpha value to create a trailing effect
-  for (let i = 0; i < ellipses.length; i++) {
-    let e = ellipses[i];
-    fill(e.color);
-    noStroke();
-    ellipse(e.x, e.y, e.size);
-    e.x += e.xSpeed;
-    e.y += e.ySpeed;
+  background(30, 28, 34);
+  
+  imageMode(CENTER);
+  
+  // Dinoperro
+  image(imgDino, windowWidth / 2, windowHeight / 2, imgDino.width * 0.8, imgDino.height * 0.8);
 
-    // Bounce off edges
-    if (e.x < 0 || e.x > width) e.xSpeed *= -1;
-    if (e.y < 0 || e.y > height) e.ySpeed *= -1;
-
-    // Change color gradually
-    e.color[0] = (e.color[0] + random(-1, 1)) % 256;
-    e.color[1] = (e.color[1] + random(-1, 1)) % 256;
-    e.color[2] = (e.color[2] + random(-1, 1)) % 256;
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  circle(x, y, 5);
+  x += dx;
+  y += dy;
 }
